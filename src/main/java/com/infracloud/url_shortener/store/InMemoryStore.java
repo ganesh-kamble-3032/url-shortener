b7de6b35bc0fa0d5b90f7ba6f023555b52568921
Ganesh Kamble <ganeshkamble9674@gmail.com>
2026-01-15 12:25:34 +0530

    Implement redirect URL API

--- src/main/java/com/infracloud/url_shortener/store/InMemoryStore.java
+++ src/main/java/com/infracloud/url_shortener/store/InMemoryStore.java
@@ -8,14 +8,11 @@ import org.springframework.stereotype.Component;
 /* Acts like an in-memory database */
 @Component
 public class InMemoryStore {
-	
-//	Short code -> Original URL
-	public Map<String ,String> shortToLongMap = new ConcurrentHashMap<>();
-	
-//	Original URl -> Short Code
-	public Map<String , String> longToShortMap = new ConcurrentHashMap<>();
-	
-//	domain -> Count
-	public Map<String , Integer> domainCountMap = new ConcurrentHashMap<>();
+
+	public Map<String, String> shortToLongMap = new ConcurrentHashMap<>();
+
+	public Map<String, String> longToShortMap = new ConcurrentHashMap<>();
+
+	public Map<String, Integer> domainCountMap = new ConcurrentHashMap<>();
 
 }
