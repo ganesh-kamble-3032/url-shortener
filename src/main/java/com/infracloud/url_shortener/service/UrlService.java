da1b086a34d1a326b3659e1e68be4d83314d9a08
Ganesh Kamble <ganeshkamble9674@gmail.com>
2026-01-15 12:23:29 +0530

    Enhance URL shortener with metrics and fixes

--- src/main/java/com/infracloud/url_shortener/service/UrlService.java
+++ src/main/java/com/infracloud/url_shortener/service/UrlService.java
@@ -21,25 +21,17 @@ public class UrlService {
 	@Autowired
 	private InMemoryStore store;
 
-	/*
-	 * Shortens a URl If URL Already exists, return existing short URL
-	 */
-
 	public String shortenurl(String originalUrl) throws Exception {
 
-		// Already exits -> return same short URL
 		if (store.longToShortMap.containsKey(originalUrl)) {
 			return BASE_URL + store.longToShortMap.get(originalUrl);
 		}
 
-		// Generate new short code
 		String shortcode = ShortCodeGenerator.generateShortCode();
 
-//        Saving mappings
 		store.longToShortMap.put(originalUrl, shortcode);
 		store.shortToLongMap.put(shortcode, originalUrl);
 
-		// Extract domain for metrics
 		URI uri = new URI(originalUrl);
 		String domain = uri.getHost();
 
@@ -47,24 +39,14 @@ public class UrlService {
 		return BASE_URL + shortcode;
 	}
 
-//		Fetch original URL for redirect
 	public String getOriginalUrl(String shortcode) {
 		return store.shortToLongMap.get(shortcode);
 
 	}
 
-//		Returns top 3 most shortened domains
-	public Map<String,Integer> getTopDomains()
-	{
-		return store.domainCountMap.entrySet()
-				.stream()
-				.sorted((a, b) -> b.getValue() - a.getValue())
-				.limit(3)
-				.collect(Collectors.toMap(
-						Map.Entry::getKey, 
-						Map.Entry::getValue,
-						(a, b) -> a,
-						LinkedHashMap::new));
+	public Map<String, Integer> getTopDomains() {
+		return store.domainCountMap.entrySet().stream().sorted((a, b) -> b.getValue() - a.getValue()).limit(3)
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a, LinkedHashMap::new));
 
 	}
 
